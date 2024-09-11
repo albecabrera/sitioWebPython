bb73e3b6b50ab0d0fc0199672b0891541ffd32eb
albecabrera <albe.cabrera@yahoo.de>
2024-09-11 21:26:57 +0200

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ## Description
 
+# sitioWebPython
+
+## Description
+
--- template/admin/sitio/app.py
+++ template/admin/sitio/app.py
@@ -1,12 +1,10 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def inicio():
     return render_template('sitio/index.html')
 
-if __name__ =='_main_':
+if __name__ == '__main__':
     app.run(debug=True)
-    
